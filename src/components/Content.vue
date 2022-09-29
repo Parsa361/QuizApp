2a39e6a55f72f1cc08ebb64d8f60bbb8bf4e3cea
Parsa361 <parsafarahany821@gmail.com>
2022-09-29 19:24:28 +0330

    Add next and submit buttons

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -12,9 +12,30 @@
             <div class="grid md:grid-cols-2 place-items-center md:mx-[-1.25rem] lg:mx-[5rem] xl:mx-[13rem]">
 
                 <!-- answer -->
-                <div class="w-[25rem] h-[6rem] rounded-lg border border-pink-600 mb-3 md:mb-[7rem] ">
+                <button class="w-[25rem] h-[6rem] rounded-lg border border-pink-600 mb-3 md:mb-[6rem] ">
                     <p class="text-center px-2 py-7">Asnwers</p>
-                </div>
+                </button>
+                <button class="w-[25rem] h-[6rem] rounded-lg border border-pink-600 mb-3 md:mb-[6rem] ">
+                    <p class="text-center px-2 py-7">Asnwers</p>
+                </button>
+                <button class="w-[25rem] h-[6rem] rounded-lg border border-pink-600 mb-3 md:mb-[6rem] ">
+                    <p class="text-center px-2 py-7">Asnwers</p>
+                </button>
+                <button class="w-[25rem] h-[6rem] rounded-lg border border-pink-600 mb-3 md:mb-[6rem] ">
+                    <p class="text-center px-2 py-7">Asnwers</p>
+                </button>
+            </div>
+
+            <!-- Buttons -->
+            <div class="flex flex-row items-center justify-center mt-5">
+                <!-- Submit button -->
+                <button class="w-[6rem] h-[3rem] bg-violet-600 mr-5">
+                    <p class="text-center px-2 py-1 text-white">Submit</p>
+                </button>
+                <!-- Next button -->
+                <button class="w-[6rem] h-[3rem] bg-green-500">
+                    <p class="text-center px-2 py-1 text-white">Next</p>
+                </button>
             </div>
         </div>
     </div>
